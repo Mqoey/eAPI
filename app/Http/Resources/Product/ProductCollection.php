4774e1b56070ec7ec6ab3801048ccb66080d2421
Mqoey <mqoeyyy@gmail.com>
2021-06-06 14:25:20 +0200

    create Product Resource

---
+++ app/Http/Resources/Product/ProductCollection.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources\Product;
+
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class ProductCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'name' => $this->name,
+            'totalPrice' => round(( 1 - ($this->discount/100)) * $this->price, 2),
+            'rating' => round($this->reviews->sum('star') / $this->reviews->count('star'), 2),
+            'href' => [
+                'link' => route('products.show', $this->id)
+            ]
+        ];
+    }
+}
